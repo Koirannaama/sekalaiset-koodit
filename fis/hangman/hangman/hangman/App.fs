d4e70f240f96a2aa5aad207755b14d75e820b523
Koirannaama <ollimatti.saarinen@gmail.com>
2016-05-27 13:41:11 +0300

    Alotettu hirsipuupeli.

---
+++ fis/hangman/hangman/hangman/App.fs
@@ -0,0 +1,13 @@
+﻿module main
+
+open System
+open FsXaml
+open ViewModels
+
+type App = XAML<"App.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+    let mv = MainView()
+    App().Root.Run(mv.Root)
---
+++ fis/hangman/hangman/hangman/MainWindow.xaml.fs
@@ -0,0 +1,23 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type MainView = XAML<"MainWindow.xaml", true>
+
+type MainViewModel() as self = 
+    inherit ViewModelBase()
+
+    let toDashes word =
+        String.map (fun c -> '-') word
+    
+    let secretWord = self.Factory.Backing(<@ self.SecretWord @>, "")
+    let progress = self.Factory.Backing(<@ self.Progress @>, "")
+    let setSecret() = progress.Value <- toDashes secretWord.Value
+
+    member this.SecretWord with get() = secretWord.Value and set(value) = secretWord.Value <- value
+    member this.Progress with get() = progress.Value and set(value) = progress.Value <- value
+    member this.SetSecret = self.Factory.CommandSync(setSecret)
