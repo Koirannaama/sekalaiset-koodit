2951bb873505a7f297d839611a614d19eaadb12c
Koirannaama <ollimatti.saarinen@gmail.com>
2016-05-29 17:32:30 +0300

    Vähän pelimekaniikkaa hirsipuulle.

--- fis/hangman/hangman/hangman/MainWindow.xaml.fs
+++ fis/hangman/hangman/hangman/MainWindow.xaml.fs
@@ -13,11 +13,25 @@ type MainViewModel() as self =
 
     let toDashes word =
         String.map (fun c -> '-') word
+
+    let checkGuess guess word =
+        String.map (fun c -> if c = guess then c else '-') word
+
+    let checkProgress progress hits =
+        Seq.zip hits progress 
+        |> Seq.map (fun t -> if (fst t) <> '-' then (fst t) else (snd t))
+        |> String.Concat 
     
     let secretWord = self.Factory.Backing(<@ self.SecretWord @>, "")
     let progress = self.Factory.Backing(<@ self.Progress @>, "")
+    let guess = self.Factory.Backing(<@ self.Guess @>, "")
+
     let setSecret() = progress.Value <- toDashes secretWord.Value
+    let doGuess() = 
+        progress.Value <- checkProgress progress.Value <| checkGuess (guess.Value.Chars 0) secretWord.Value
 
     member this.SecretWord with get() = secretWord.Value and set(value) = secretWord.Value <- value
     member this.Progress with get() = progress.Value and set(value) = progress.Value <- value
     member this.SetSecret = self.Factory.CommandSync(setSecret)
+    member this.Guess with get() = guess.Value and set(value) = guess.Value <- value
+    member this.DoGuess = self.Factory.CommandSync(doGuess)
