daec3e25e7a7fc9da3b8f35d25a46b7bb420f879
Koirannaama <ollimatti.saarinen@gmail.com>
2016-06-05 15:20:40 +0300

    Hirsipuu muistuttaa jo peliä.

--- fis/hangman/hangman/hangman/App.fs
+++ fis/hangman/hangman/hangman/App.fs
@@ -6,8 +6,17 @@ open ViewModels
 
 type App = XAML<"App.xaml">
 
+let isNotEmpty text =
+    text <> ""
+
+let init (mv: MainView) =
+    mv.SubmitGuess.IsEnabled <- false
+    mv.Guess.TextChanged
+    |> Event.add (fun _ -> mv.SubmitGuess.IsEnabled <- isNotEmpty mv.Guess.Text)
+    mv
+
 [<STAThread>]
 [<EntryPoint>]
 let main argv =
-    let mv = MainView()
+    let mv = init(MainView())
     App().Root.Run(mv.Root)
