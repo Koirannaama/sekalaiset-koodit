8fa2d4ee1b5ca4e14bb67c1aae944377e5830a01
Koirannaama <ollimatti.saarinen@gmail.com>
2017-01-14 20:54:48 +0200

    Lisätty bussimonitoriin pysäkkiehdotukset.

--- bussit/bussit.js
+++ bussit/bussit.js
@@ -41,16 +41,39 @@ function getArrivals(arrivalData) {
 }
 
 angular.module("bussiApp", [])
-  .controller("BusController", function ($http, $interval){
+  .controller("BusController", function ($http, $interval) {
     var arrivals = this;
-    arrivals.arrivalApiUrl = "http://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=";
-    arrivals.stopCode = "4517";
+    arrivals.stops = {};
+
+    var stopsApiUrl = "http://data.itsfactory.fi/journeys/api/1/stop-points";
+    var arrivalApiUrl = "http://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=";
+    $http.get(stopsApiUrl)
+    .then(function(res) {
+      var stopData = res.data.body;
+      stopData.forEach(function(stop) {
+        arrivals.stops[stop.name] = stop.shortName;
+      });
+    });
+
+    //arrivals.arrivalApiUrl = "http://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=";
+    arrivals.stopName = "Sammonkatu 47";
+    arrivals.shadowStopName = arrivals.stopName;
     arrivals.errorText = "";
 
+    arrivals.submitRequest = function() {
+      arrivals.stopName = arrivals.shadowStopName;
+      arrivals.getArrivalTimes();
+    };
+
     arrivals.getArrivalTimes = function() {
-      $http.get(arrivals.arrivalApiUrl + arrivals.stopCode)
+      if (arrivals.stops[arrivals.stopName] === undefined) {
+        return;
+      }
+      var stopCode = arrivals.stops[arrivals.stopName];
+
+      $http.get(arrivalApiUrl + stopCode)
       .then(function(res) {
-        var arrivalData = res.data.body[arrivals.stopCode];
+        var arrivalData = res.data.body[stopCode];
         var arrivalTimes = getArrivals(arrivalData);
         if (arrivals.length === 0) {
           arrivals.arrivalTimes = [];
